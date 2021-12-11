7a7e1461b229cc279b465151cdbc342383ffaf50
Meet <meetmakwana203.@gmail.com>
2021-12-11 23:40:36 +0530

    Commit 8-16

--- src/App.js
+++ src/App.js
@@ -1,16 +1,82 @@
 import './App.css';
+import About from './components/About';
 import Navbar from './components/Navbar';
 import Textform from './components/Textform';
+import React, { useState } from 'react'
+import Alert from './components/Alert';
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Link
+} from "react-router-dom";
 
 function App() {
+
+    const [mode, setMode] = useState('light'); // `mode` is a state variable telling whether dark mode is enabled or not. `light` means by default dar mode is disabled.
+
+    const [alert, setAlert] = useState(null); //alert will be by default null
+    const showAlert = (message, type) => { //showAlert func will take 2 parameters- message, type
+        // will try to set alert using  this func where setAlert here is the object.
+        setAlert({
+            msg: message,
+            type: type
+        })
+        // will remove the showAlert after 3seconds
+        setTimeout(() => {
+            setAlert(null);
+        }, 2000)
+    }
+
+    const toggleMode = () => {
+        if (mode === 'light') {
+            setMode('dark');
+            document.body.style.backgroundColor = "#203c59";
+
+            // this will show the alert with custom msg, useState rockks
+            showAlert("Dark mode has been enabled", "success")
+
+            // To change the title 
+            document.title = "TextUtils-Dark Mode"
+
+            //just for knowledge purpose : If want to flash something say in the title then do like the following
+            // setInterval(() => {
+            //     document.title = "TextUtils-Install"
+            // }, 2000);
+            // setInterval(() => {
+            //     document.title = "TextUtils-Virus"
+            // }, 1500);
+        }
+        else {
+            setMode('light');
+            document.body.style.backgroundColor = "white";
+            showAlert("Light mode has been enabled", "success")
+            document.title = "TextUtils-Light Mode"
+
+        }
+    }
     return (
         <>
-            <Navbar title="Text-Utils" about="About" />
-            {/* <Navbar  /> */}
+            <Router>
+                <Navbar title="Text-Utils" about="About" mode={mode} toggleMode={toggleMode} /> {/*mode=light/date willl be sent here using states. */}
+                {/* <Navbar  /> */}
 
-            <div className="container my-3">
-                <Textform heading="Enter the text to analyze below" />
-            </div>
+                <Alert alert={alert} />
+
+                <div className="container my-3">
+                    {/* A <Routes> looks through its children <Route>s and
+                    renders the first one that matches the current URL. */}
+                    <Routes>
+                        {/* Can use `exact` with `path` as `exact path` in the `<Route>` to do exact routing otherwise react does partial routing */}
+                        <Route exact path="/about" element={<About/>}>
+                            {/* <About/> */}
+                        </Route>
+                        <Route exact path="/" element={<Textform heading="Enter the text to analyze below" mode={mode} showAlert={showAlert} />}>
+                            {/* <Textform heading="Enter the text to analyze below" mode={mode} showAlert={showAlert} /> */}
+                        </Route>
+                    </Routes>
+                </div>
+            </Router>
         </>
     );
 }
--- src/components/Textform.js
+++ src/components/Textform.js
@@ -19,35 +19,54 @@ export default function Textform(props) {
         let newText = text.toUpperCase();
         // setText("You've clicked on handleUpperCaseClick") /* this is to change the state */
         setText(newText)
+        props.showAlert("Converted to Uppercase", "success")
     }
     const handleLowerCaseClick = ()=>{
         let newText = text.toLowerCase();
         setText(newText)
+        props.showAlert("Converted to Lowercase", "success")
     }
     const handleClearTextClick = ()=>{
         let newText = "";
         setText(newText)
     }
+    
     const handleOnChange = (event)=>{
         // console.log("Onchange");
         setText(event.target.value); //helps for typing input, also onChange funt will be fired for every character entry
         //means making the the value={text} to whatever is typed 
         //setting the default text value to the new setText.
     }
+    
+    const handleCopy = ()=>{
+        var text = document.getElementById("myBox")
+        text.select();
+        navigator.clipboard.writeText(text.value) 
+        props.showAlert("Copied to clipboard", "success")
+    }
+    
+    const handleExtraSpace = ()=>{
+        // using regex
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(" "));
+        props.showAlert("Handed extra spaces", "success")
+    }
 
     return (
         <>
-        <div className="container">
+        <div className="container" style={{color:props.mode === "dark" ? "white":"black"}}>
             <h1>{props.heading}</h1>
             <div className="mb-3">
-                <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+                <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode === "dark" ? "#203c59":"white", color: props.mode === "dark" ? "white":"black"}} id="myBox" rows="8"></textarea>
             </div>
             <button className="btn btn-primary" onClick={handleUpperCaseClick}>Convert to Uppercase</button>
             <button className="btn btn-primary mx-2" onClick={handleLowerCaseClick}>Convert to Lowercase</button>
             <button className="btn btn-primary mx-2" onClick={handleClearTextClick}>Clear Text</button>
+            <button className="btn btn-primary mx-2" onClick={handleCopy}>Copy</button>
+            <button className="btn btn-primary mx-2" onClick={handleExtraSpace}>Remove extra spaces</button>
         </div>
         
-        <div className="container my-3">
+        <div className="container my-3" style={{color: props.mode === "dark" ? "white":"black"}}>
 
             <h2>Your text summary</h2>
 
